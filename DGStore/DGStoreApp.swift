0b06610426e98d77fc65b775d882f925e8342347
이석호 <mercen@mercen.net>
2022-07-18 23:06:15 +0900

    Initial Commit

---
+++ DGStore/DGStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  DGStoreApp.swift
+//  DGStore
+//
+//  Created by Mercen on 2022/07/18.
+//
+
+import SwiftUI
+
+@main
+struct DGStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
